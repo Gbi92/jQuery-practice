da7b940c9340f476e0073fc17eea5fd221995e41
Gbi92 <70910864+Gbi92@users.noreply.github.com>
2020-09-14 21:13:13 +0200

    changed content of boxes

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let color = 'purple';
+let number = 10;
+let word = "cool";
+
+if (color === 'purple'){
+    $('.north').css({background:'purple'});
+}
+
+if (number > 100){
+    $('.east').html("whoah, that's a big number");
+} else {
+    $('.east').html("just a regular number, please");
+}
+
+if (word === "cool"){
+    $('.south').html("Power of DOM");
+} else {
+    $('.south').html("Power of DOM");
+}
